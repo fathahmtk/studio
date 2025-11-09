3df27c67cfdd117d6d4f68ccab195c8ec4661e56
Abdul Fathah MTK <abdulfathahmtk@gmail.com>
2025-11-09 17:52:16 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-1992761061-285f2",
+  "appId": "1:720012269089:web:eb3bb2d6d805e4a7b8f560",
+  "apiKey": "AIzaSyA2rHnO0jXqNTCQTiETSdjDAFSlGOVK3k0",
+  "authDomain": "studio-1992761061-285f2.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "720012269089"
+};
